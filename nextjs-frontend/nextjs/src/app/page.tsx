9b515cf2e10f615a672fc5ca68c44783bc961d43
Mateus S. Neubarth <mateusneubarth@hotmail.com>
2024-06-21 18:22:04 -0300

    criado frontend com nextJS

---
+++ nextjs-frontend/nextjs/src/app/page.tsx
@@ -0,0 +1,32 @@
+import { Title } from "../components/Title";
+import { EventCard } from "../components/EventCard";
+import { EventModel } from "@/model";
+
+export async function getEvents(): Promise<EventModel[]> {
+  const response = await fetch(`http://localhost:8080/events`, {
+    // headers: {
+    //   apikey: process.env.GOLANG_API_TOKEN as string,
+    // },
+    cache: "no-store",
+    // next: {
+    //   tags: ["events"],
+    // }
+  });
+
+  return (await response.json()).events;
+}
+
+export default async function HomePage() {
+  const events = await getEvents();
+  console.log(events);
+  return (
+    <main className="mt-10 flex flex-col">
+      <Title>Eventos disponíveis</Title>
+      <div className="mt-8 sm:grid sm:grid-cols-auto-fit-cards flex flex-wrap justify-center gap-x-2 gap-y-4">
+        {events.map((event) => (
+          <EventCard key={event.id} event={event} />
+        ))}
+      </div>
+    </main>
+  );
+}
